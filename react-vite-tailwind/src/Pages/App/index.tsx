53c4c5bdee66f710badc2fc289fc3b15c062a927
Tomás <tomasleggire@hotmail.com>
2023-10-05 21:10:30 -0300

    React router dom installed and implemented

--- react-vite-tailwind/src/Pages/App/index.tsx
+++ react-vite-tailwind/src/Pages/App/index.tsx
@@ -1,22 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
 import SingIn from '../SingIn'
-
+import NotFound from '../NotFound'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+    const routes = useRoutes([
+        { path: '/', element: <Home /> },
+        { path: '/my-account', element: <MyAccount /> },
+        { path: '/my-order', element: <MyOrder /> },
+        { path: '/my-orders', element: <MyOrders /> },
+        { path: '/my-orders', element: <MyOrders /> },
+        { path: '/sing-in', element: <SingIn /> },
+        { path: '/*', element: <NotFound /> },
+    ])
+    return routes;
+}
+
+const App = () => {
   return (
-    <div className='bg-red-300'>
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SingIn />
-    </div>
+   <BrowserRouter>
+    <AppRoutes />
+   </BrowserRouter>
   )
 }
 
