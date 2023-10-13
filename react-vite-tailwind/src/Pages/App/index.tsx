6c8561565e4393387ddfaea3111dcd93839928f5
Tomás <tomasleggire@hotmail.com>
2023-10-13 20:25:07 -0300

    Navbar creado

--- react-vite-tailwind/src/Pages/App/index.tsx
+++ react-vite-tailwind/src/Pages/App/index.tsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import SingIn from '../SingIn'
 import NotFound from '../NotFound'
+import { Navbar } from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
@@ -24,6 +25,7 @@ const App = () => {
   return (
    <BrowserRouter>
     <AppRoutes />
+    <Navbar />
    </BrowserRouter>
   )
 }
